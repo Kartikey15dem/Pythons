34e454435534733dd714bcf8dabfbf85a3e28f74
Kartikey15dem <kartikeyshukla15@gmail.com>
2023-11-01 16:35:24 +0530

    Update pr.py

--- pr.py
+++ pr.py
@@ -2,9 +2,13 @@ number=input("Enter any natural number other than 1: ")
 prime=[]
 num=int(number)
 for i in range(2,num+1):
+    pri=True
     if num%i==0:
-        for j in range(2,int(i/2)+1):
+        for j in range(2,i):
             if i%j==0:
-               prime.append(j)
+               pri=False
+               break
+        if pri==True:
+            prime.append(i)
 print("The set of prime factors for the given number is: ")
 print(prime)
